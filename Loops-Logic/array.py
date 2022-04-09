25184c940d3e3450224546a82d5ec7a2e23a6608
Carl Shaw <101380344+Hobo-Hilly@users.noreply.github.com>
2022-04-09 13:10:24 -0700

    Update array.py

--- Loops-Logic/array.py
+++ Loops-Logic/array.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-#Declare a list/array
+#Declare a list/array after awesome starwars characters
 array1 = ["Luke", "Han", "Chewy", "R2", "3PO", "Ben"]
 
 
 #Print specific index
 print array1[3]     # so this is indexing or 'calling' position 3 of 'array1'
-print array1[5]
-print array1[0]
+print array1[5]     # so this is indexing or 'calling' position 5 of 'array1'
+print array1[0]     # so this is indexing or 'calling' position 0 of 'array1'
 
 
 
