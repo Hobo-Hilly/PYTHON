ca516b316b407ff4d312a6e18bbfcbfc1ba04a8c
Carl Shaw <101380344+Hobo-Hilly@users.noreply.github.com>
2022-04-09 12:56:39 -0700

    Update array.py

--- Loops-Logic/array.py
+++ Loops-Logic/array.py
@@ -5,7 +5,7 @@ array1 = ["Luke", "Han", "Chewy", "R2", "3PO", "Ben"]
 
 
 #Print specific index
-print array1[3]
+print array1[3]     # so this is indexing or 'calling' position 3 of 'array1'
 print array1[5]
 print array1[0]
 
