e97ffd5130b8bc21f664db323d6a61dc337596cd
Carl Shaw <101380344+Hobo-Hilly@users.noreply.github.com>
2022-12-15 16:49:30 -0800

    Update py_compare.py

--- Loops-Logic/py_compare.py
+++ Loops-Logic/py_compare.py
@@ -2,9 +2,9 @@
 
 var = raw_input("Are you a human? [1]Yes, [2]No ")
 if var == "1":
-    print "Greetings, Earthling"
+    print ("Greetings, Earthling")
 elif var == "2":
-    print "Welcome to Earth"
+    print ("Welcome to Earth")
 else:
-    print "I've got a bad feeling about this"
+    print ("I've got a bad feeling about this")
 
